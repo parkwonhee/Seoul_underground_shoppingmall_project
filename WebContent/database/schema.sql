4aa2aba4fcc80ad6194c31616688274ed967e93a
parkwonhee <38245863+parkwonhee@users.noreply.github.com>
2019-02-13 20:27:48 +0900

    Update schema.sql
    add reamark

--- WebContent/database/schema.sql
+++ WebContent/database/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS mall
 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE mall;
 
-CREATE TABLE IF NOT EXISTS  admin(
+CREATE TABLE IF NOT EXISTS  admin(//로그인 정보 저장
     id VARCHAR(32) PRIMARY KEY,
     password VARCHAR(32)
 );
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS  admin(
 INSERT INTO admin VALUES ('won','1234');
 INSERT INTO admin VALUES ('sue','1234');
 
-create table IF NOT EXISTS admingo(뇆
+create table IF NOT EXISTS admingo(
 	cmd varchar(10) not null,
 	area varchar(10) not null,
 	name varchar(30) PRIMARY KEY not null
@@ -28,7 +28,7 @@ INSERT INTO adminifc VALUES('2','c','starbucks');
 
 ALTER TABLE admin CHARACTER SET utf8 COLLATE utf8_general_ci; 
 ALTER TABLE admingo CHARACTER SET utf8 COLLATE utf8_general_ci; 
-ALTER TABLE adminifc CHARACTER SET utf8 COLLATE utf8_general_ci; 
+ALTER TABLE adminifc CHARACTER SET utf8 COLLATE utf8_general_ci; //한글
 
 
 
